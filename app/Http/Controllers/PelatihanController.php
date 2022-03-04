3c5fce26ee5e67162f2ef3c4800f0ddbe04bffbd
FebriTahta <febrirtah@gmail.com>
2022-03-03 17:52:43 -0800

    oke

--- app/Http/Controllers/PelatihanController.php
+++ app/Http/Controllers/PelatihanController.php
@@ -85,7 +85,7 @@ class PelatihanController extends Controller
                     $cetak = ' <a data-target="#modalhapus" data-id="'.$data->id.'" data-name="'.$data->name.'" data-toggle="modal" href="javascript:void(0)" class="delete btn bg-pink waves-effect btn-sm"><i class="material-icons">print</i></a>';
                     return $cetak;
                 })
-                ->rawColumns(['option','edit'])
+                ->rawColumns(['option','edit','cetak'])
                 ->make(true);
         }
     }
