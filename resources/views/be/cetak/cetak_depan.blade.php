a05a6718ac420218564eca7ee26358b4a93e7dc8
FebriTahta <febrirtah@gmail.com>
2022-09-27 10:47:27 +0700

    cetak syahadah versi lama

--- resources/views/be/cetak/cetak_depan.blade.php
+++ resources/views/be/cetak/cetak_depan.blade.php
@@ -7,18 +7,18 @@
 	<style>
 		body{
 			font-family: Arial, Helvetica, sans-serif;
-			font-size: 16px;
+			font-size: 14px;
 		}
 		.page-break {
 			page-break-after: always;
 			page-break-inside: avoid;
 		}
 		.atas {
-			font-size: 16px;
+			font-size: 14px;
 			font-family: Arial, Helvetica, sans-serif;
 		}
 		.bawah {
-			font-size: 12px;
+			font-size: 14px;
 			font-family: Arial, Helvetica, sans-serif;
 		}
 		.dalam{
@@ -30,80 +30,102 @@
 		@foreach ($peserta as $item)
 		<div class="print">
 			<table 
-			style="height: 180px; width: 900px;margin-left:100px;margin-top:350px"
+			style="height: 180px; width: 900px;margin-left:138px;margin-top:287px"
 			class="dalam">
 				<tbody>
-				<tr class="atas" style="height: 23px;">
-				<td class="atas" style="width: 170px; height: 23px;">&nbsp;</td>
-				<td class="atas" style="width: 11px; height: 23px;"></td>
-				{{-- Output nama dengan gelar --}}
-				<td class="atas" style="width: 750px; height: 23px;">{{ $item->nama }}</td>
-				<td class="atas" style="width: 52px; height: 23px; ">&nbsp;</td>
+				<tr class="atas">
+                    <td class="atas" style="width: 170px; height: 23px;">&nbsp;</td>
+                    <td class="atas" style="width: 11px; height: 23px;"></td>
+                    <td class="atas" style="width: 750px; height: 23px;">{{ $item->nama }}</td>
+                    <td class="atas" style="width: 52px; height: 23px; ">&nbsp;</td>
 				</tr>
-				<tr class="atas" style="height: 23px;">
-				<td class="atas" style="width: 170px; height: 23px;"></td>
-				<td class="atas" style="width: 11px; height: 23px;"></td>
-				<td class="atas" style="width: 750px; height: 23px; text-transform: uppercase" >{{ $item->alamat }} {{$item->kota}}</td>
-				<td class="atas" style="width: 52px; height: 23px;">&nbsp;</td>
+				<tr class="atas">
+                    <td class="atas" style="width: 170px; height: 23px;"></td>
+                    <td class="atas" style="width: 11px; height: 23px;"></td>
+                    <td class="atas" style="width: 750px; height: 23px; text-transform: uppercase" >
+
+						{{ $item->alamat }}
+						
+					
+					</td>
+                    <td class="atas" style="width: 52px; height: 23px;">&nbsp;</td>
 				</tr>
-				<tr class="atas" style="height: 23px;">
-				<td class="atas" style="width: 170px; height: 23px; "></td>
-				<td class="atas" style="width: 11px; height: 23px;"></td><?php date_default_timezone_set('Asia/Jakarta'); $date=$item->tgllahir;?>
-				<td class="atas" style="width: 750px; height: 23px; text-transform: uppercase" >{{ $item->tmplahir }}, {{ Carbon\Carbon::parse($date)->isoFormat('D MMMM Y') }}&nbsp;</td>
-				<td class="atas" style="width: 52px; height: 23px;">&nbsp;</td>
+				<tr class="atas">
+                    <td class="atas" style="width: 170px; height: 23px; "></td>
+                    <td class="atas" style="width: 11px; height: 23px;"></td><?php date_default_timezone_set('Asia/Jakarta'); $date=$item->tgllahir;?>
+                    <td class="atas" style="width: 750px; height: 23px; text-transform: uppercase" >
+						{{ $item->tmplahir }}, {{ Carbon\Carbon::parse($date)->isoFormat('D MMMM Y') }}&nbsp;	
+					</td>
+                    <td class="atas" style="width: 52px; height: 23px;">&nbsp;</td>
 				</tr>
 				
-				<tr class="atas" style="height: 23px;">
-				<td class="atas" style="width: 170px; height: 23px;"></td>
-				<td class="atas" style="width: 11px; height: 23px;"></td>
-				<td class="atas" style="width: 750px; height: 23px; text-transform: uppercase" >{{ $item->kriteria }}</td>
-				<td class="atas" style="width: 52px; height: 23px;">&nbsp;</td>
+				<tr class="atas">
+                    <td class="atas" style="width: 170px; height: 23px;"></td>
+                    <td class="atas" style="width: 11px; height: 23px;"></td>
+                    <td class="atas" style="width: 750px; height: 23px; text-transform: uppercase" >{{ $item->kriteria }}</td>
+                    <td class="atas" style="width: 52px; height: 23px;">&nbsp;</td>
 				</tr>
-				<tr class="atas" style="height: 37px;">
-				<td class="atas" style="width: 170px; height: 37px;">&nbsp;</td>
-				<td class="atas" style="width: 11px; height: 37px;">&nbsp;</td>
-				<td class="atas" style="width: 750px; height: 37px;">&nbsp;</td>
-				<td class="atas" style="width: 52px; height: 37px;">&nbsp;</td>
+				<tr class="atas">
+                    <td class="atas" style="width: 170px; height: 37px;">&nbsp;</td>
+                    <td class="atas" style="width: 11px; height: 37px;">&nbsp;</td>
+                    <td class="atas" style="width: 750px; height: 37px;">&nbsp;</td>
+                    <td class="atas" style="width: 52px; height: 37px;">&nbsp;</td>
 				</tr>
 				</tbody>
 			</table>			
 			<table 
 		
-			style="margin-left:113px; margin-top: 4px"
-			
+			style="margin-left:169px; margin-top: 15px"
 			>
 				<tbody>
 				<tr style="height: 27px;"><?php $tahun = date('Y')?>
-				<td class="bawah" style="width: 210px; height: 27px; "><small> </small></td>
-				<td class="bawah" style="width: 210px; height: 27px;">&nbsp;</td><?php $lokasicetak = strtolower($item->pelatihan->cabang->kota)?>
-				<td class="atas" style="width: 241px; height: 27px; text-transform: lowercase;text-transform: capitalize">{{$lokasicetak}}, {{ Carbon\Carbon::parse($item->pelatihan->tanggal)->isoFormat('D MMMM Y') }}</td>
+					{{-- kosong --}}
+					<td class="bawah" style="width: 210px; height: 27px;  "><small> </small></td>
+					{{-- kosong --}}
+					<td class="bawah" style="width: 156px; height: 27px;  ">&nbsp;</td>
+					{{-- tanggal pelaksanaan --}}
+					<?php $lokasicetak = strtolower($item->pelatihan->cabang->kota)?>
+					<td class="atas" style="width: 241px; height: 27px; text-transform: lowercase;text-transform: capitalize;  ">Surabaya, {{ Carbon\Carbon::parse($item->pelatihan->tanggal)->isoFormat('D MMMM Y') }}</td>
 				</tr>
 				<tr style="height: 78px;">
-				<td class="bawah" style="width: 210px; height: 70px;">
-					<div class="tepi" style="width: 70px; padding: 2px">
+					{{-- QR --}}
+					<td class="bawah" style="width: 210px; height: 70px; ">
+						<div class="tepi" style="width: 70px; padding: 2px; ">
+							{{-- <img src="images/{{ $item->slug }}.png" alt="" width="70px" height="70px"> --}}
+						</div>
+					</td>
+					{{-- kosong --}}
+					<td class="bawah" style="width: 156px; height: 70px; ">&nbsp;</td>
+					{{-- kosong --}}
+					<td class="bawah" style="width: 241px; height: 70px; ">
 						
-					</div>
-				</td>
-				<td class="bawah" style="width: 210px; height: 70px;">&nbsp;</td>
-				<td class="bawah" style="width: 241px; height: 70px;">
-					
-				</td>
+					</td>
 				</tr>
 				<tr style="height: 5px;">
-				<td class="bawah" style="width: 210px; height: 5px;">No. Syahadah : &nbsp;{{ $item->pelatihan_id }}/{{ $tahun }}/{{ $item->id }}</td>
-				<td class="bawah" style="width: 210px; height: 5px;">&nbsp;</td>
-				<td class="atas" style="width: 241px; height: 5px;"> {{$item->pelatihan->cabang->nmkepala}} </td>
+					{{-- no syahadah --}}
+					<td class="bawah" style="width: 210px; height: 5px;">No. Syahadah : &nbsp;{{ $item->pelatihan_id }}/{{ $tahun }}/{{ $item->id }}</td>
+					{{-- kosong --}}
+					<td class="bawah" style="width: 156px; height: 5px;">&nbsp;</td>
+					{{-- direktur --}}
+					<td class="atas" style="width: 241px; height: 5px;"> 
+						<u>
+							{{$item->pelatihan->cabang->nmkepala}}
+						</u>
+					</td>
 				</tr>
 				<tr style="height: 4px;">
-				<td class="bawah" style="width: 210px; height: 4px;">&nbsp;</td>
-				<td class="bawah" style="width: 185px; height: 4px;">&nbsp;</td>
-				<td class="bawah" style="width: 241px; height: 2px; text-transform: capitalize"> 
-                    @if ($lokasicetak == 'surabaya')
-                        Direktur Eksekutif
-                    @else
-                        Kacab. {{$lokasicetak}}
-                    @endif  
-                </td>
+					{{-- kosong --}}
+					<td  style="width: 210px; height: 4px;  ">&nbsp;</td>
+					{{-- kosong --}}
+					<td  style="width: 156px; height: 4px;  ">&nbsp;</td>
+					{{-- kepala cabang mana --}}
+					<td style="width: 241px; height: 2px; text-transform: capitalize; font-size: 13px"> 
+						@if ($lokasicetak == 'surabaya')
+                        	Direktur Eksekutif
+						@else
+							Kacab. {{$lokasicetak}}
+						@endif 
+					</td>
 				</tr>
 				</tbody>
 			</table>
@@ -111,4 +133,5 @@
 		@endforeach
 </body>
 </html>	
+
 
